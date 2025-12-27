4995517a51e1fe9faff59eeb8d112da45a81b3dd
Basudha Mahapatra <basudha.rksmsngs@gmail.com>
2025-12-27 09:53:20 +0530

    Updated file

--- TIM RoughGarden/MergeSort.java
+++ TIM RoughGarden/MergeSort.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class MergeSort {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for(int i =0; i <n; i++){
+            System.out.print("Enter the " + i+1 + "th element: ");
+            arr[i] = sc.nextInt();
+        }
+    }
+
+
+    void mergeSort(int arr[], int U, int L, int n){
+        M = (U+L)/2;
+        if
+
     }
 }
