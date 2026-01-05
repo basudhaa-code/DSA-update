d8a9e71a872fc92f4b44674ebe06e1701b15fed7
Basudha Mahapatra <basudha.rksmsngs@gmail.com>
2026-01-05 19:10:28 +0530

    all the updates

---
+++ queue and stack/NearestGreaterLeft.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class NearestGreaterLeft {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int result[] = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("output: ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i]);
+        }
+
+        System.out.println("\n");
+
+        for(int i = 0; i < n; i++){
+            while(!stack.isEmpty() && stack.peek() <= arr[i])
+                stack.pop();
+            if(stack.isEmpty())
+                result[i] = -1;
+            else
+                result[i] = stack.peek();
+
+            stack.push(arr[i]);
+
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.print(result[i]);
+        }
+
+
+        
+    }
+    
+}
